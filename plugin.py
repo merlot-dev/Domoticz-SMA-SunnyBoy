102607aea83bd35d0240970fda646f55f83352c4
Frank van Doorn <fvdoorn@antarc.com>
2020-10-02 10:59:52 +0200

    Reformatted code

--- plugin.py
+++ plugin.py
@@ -36,6 +36,7 @@
 """
 
 import sys
+
 sys.path.append('/usr/local/lib/python3.5/dist-packages/')
 
 try:
@@ -45,6 +46,7 @@ except ImportError:
 
 import json
 import requests
+
 
 class BasePlugin:
     enabled = False
@@ -57,19 +59,19 @@ class BasePlugin:
         return
 
     def onStart(self):
-       Domoticz.Log("onStart called")
-       if Parameters["Mode6"] == "Debug":
+        Domoticz.Log("onStart called")
+        if Parameters["Mode6"] == "Debug":
             Domoticz.Debugging(1)
-       else:
+        else:
             Domoticz.Debugging(0)
-       if (len(Devices) == 0):
-         Domoticz.Device(Name="PV Generation", Unit=1, TypeName="General", Subtype=29).Create()
-         Domoticz.Device("kWh total", 2, "Custom", Options = { "Custom" : "1;kWh"}).Create()
-       DumpConfigToLog()
-       Domoticz.Log("Plugin is started.")
-# If Heartbeat>30 you'll get the error thread seems to have ended unexpectedly
-# https://www.domoticz.com/wiki/Developing_a_Python_plugin#Callbacks
-       Domoticz.Heartbeat(20)
+        if (len(Devices) == 0):
+            Domoticz.Device(Name="PV Generation", Unit=1, TypeName="General", Subtype=29).Create()
+            Domoticz.Device("kWh total", 2, "Custom", Options={"Custom": "1;kWh"}).Create()
+        DumpConfigToLog()
+        Domoticz.Log("Plugin is started.")
+        # If Heartbeat>30 you'll get the error thread seems to have ended unexpectedly
+        # https://www.domoticz.com/wiki/Developing_a_Python_plugin#Callbacks
+        Domoticz.Heartbeat(20)
 
     def onStop(self):
         Domoticz.Log("onStop called")
@@ -81,103 +83,116 @@ class BasePlugin:
         Domoticz.Log("onMessage called")
 
     def onCommand(self, Unit, Command, Level, Hue):
-        Domoticz.Log("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
+        Domoticz.Log(
+            "onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
 
     def onNotification(self, Name, Subject, Text, Status, Priority, Sound, ImageFile):
-        Domoticz.Log("Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(Priority) + "," + Sound + "," + ImageFile)
+        Domoticz.Log("Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(
+            Priority) + "," + Sound + "," + ImageFile)
 
     def onDisconnect(self, Connection):
         Domoticz.Log("onDisconnect called")
 
     def onHeartbeat(self):
-      Domoticz.Log("onHeartbeat called "+ str(self.lastPolled))
-      ## Read SMA Inverter ##
-      Domoticz.Log(str(Parameters))
-      url_base="https://" + Parameters["Address"] + "/dyn/"
-      url=url_base + "login.json"
-      payload = ('{"pass" : "' + Parameters["Password"] + '", "right" : "usr"}')
-      headers = {'Content-Type': 'application/json', 'Accept-Charset': 'UTF-8'}
+        Domoticz.Log("onHeartbeat called " + str(self.lastPolled))
+        ## Read SMA Inverter ##
+        Domoticz.Log(str(Parameters))
+        url_base = "https://" + Parameters["Address"] + "/dyn/"
+        url = url_base + "login.json"
+        payload = ('{"pass" : "' + Parameters["Password"] + '", "right" : "usr"}')
+        headers = {'Content-Type': 'application/json', 'Accept-Charset': 'UTF-8'}
 
-      self.lastPolled = self.lastPolled + 1
-      if (self.lastPolled > (3*int(Parameters["Mode3"]))): self.lastPolled = 1
-      if (self.lastPolled == 1):
-        try:
-          r = requests.post(url, data=payload, headers=headers, verify=self.verify_key)
-        except Exception as e:
-          Domoticz.Log("Error accessing SMA inverter on "+Parameters["Address"] + " with error " + str(e))
-        else:
-          j = json.loads(r.text)
-          try:
-            sid = j['result']['sid']
-          except:
-            Domoticz.Log("No response from SMA inverter on "+Parameters["Address"])
-          else:
-            url = url_base + "getValues.json?sid=" + sid
-            payload = ('{"destDev":[],"keys":["6400_00260100","6400_00262200","6100_40263F00"]}')
-            headers = {'Content-Type': 'application/json', 'Accept-Charset': 'UTF-8'}
-
+        self.lastPolled = self.lastPolled + 1
+        if (self.lastPolled > (3 * int(Parameters["Mode3"]))): self.lastPolled = 1
+        if (self.lastPolled == 1):
             try:
-              r = requests.post(url, data=payload, headers=headers, verify=self.verify_key)
-            except:
-              Domoticz.Log("No data from SMA inverter on "+Parameters["Address"])
+                r = requests.post(url, data=payload, headers=headers, verify=self.verify_key)
+            except Exception as e:
+                Domoticz.Log("Error accessing SMA inverter on " + Parameters["Address"] + " with error " + str(e))
             else:
-              j = json.loads(r.text)
-              try:
-                  sma_data = j['result'][Parameters['Mode2']]
-              except:
-                  Domoticz.Log("Possible wrong serial. Expected serial: "+Parameters['Mode2'] + " response: " + r.text)
-              else:
-                  sma_pv_watt = sma_data['6100_40263F00']['1'][0]['val']
-                  if sma_pv_watt is None:
-                    sma_pv_watt = 0
-                  sma_kwh_today = sma_data['6400_00262200']['1'][0]['val']
-                  sma_kwh_total = sma_data['6400_00260100']['1'][0]['val']/1000
+                j = json.loads(r.text)
+                try:
+                    sid = j['result']['sid']
+                except:
+                    Domoticz.Log("No response from SMA inverter on " + Parameters["Address"])
+                else:
+                    url = url_base + "getValues.json?sid=" + sid
+                    payload = ('{"destDev":[],"keys":["6400_00260100","6400_00262200","6100_40263F00"]}')
+                    headers = {'Content-Type': 'application/json', 'Accept-Charset': 'UTF-8'}
 
-                  Devices[1].Update(nValue=0, sValue=str(sma_pv_watt)+";"+str(sma_kwh_today))
-                  sValue="%.2f" % sma_kwh_total
-                  Devices[2].Update(nValue=0, sValue=sValue.replace('.',','))
+                    try:
+                        r = requests.post(url, data=payload, headers=headers, verify=self.verify_key)
+                    except:
+                        Domoticz.Log("No data from SMA inverter on " + Parameters["Address"])
+                    else:
+                        j = json.loads(r.text)
+                        try:
+                            sma_data = j['result'][Parameters['Mode2']]
+                        except:
+                            Domoticz.Log("Possible wrong serial. Expected serial: " + Parameters[
+                                'Mode2'] + " response: " + r.text)
+                        else:
+                            sma_pv_watt = sma_data['6100_40263F00']['1'][0]['val']
+                            if sma_pv_watt is None:
+                                sma_pv_watt = 0
+                            sma_kwh_today = sma_data['6400_00262200']['1'][0]['val']
+                            sma_kwh_total = sma_data['6400_00260100']['1'][0]['val'] / 1000
+
+                            Devices[1].Update(nValue=0, sValue=str(sma_pv_watt) + ";" + str(sma_kwh_today))
+                            sValue = "%.2f" % sma_kwh_total
+                            Devices[2].Update(nValue=0, sValue=sValue.replace('.', ','))
 
 
 global _plugin
 _plugin = BasePlugin()
 
+
 def onStart():
     global _plugin
     _plugin.onStart()
+
 
 def onStop():
     global _plugin
     _plugin.onStop()
 
+
 def onConnect(Connection, Status, Description):
     global _plugin
     _plugin.onConnect(Connection, Status, Description)
+
 
 def onMessage(Connection, Data, Status, Extra):
     global _plugin
     _plugin.onMessage(Connection, Data, Status, Extra)
 
+
 def onCommand(Unit, Command, Level, Hue):
     global _plugin
     _plugin.onCommand(Unit, Command, Level, Hue)
+
 
 def onNotification(Name, Subject, Text, Status, Priority, Sound, ImageFile):
     global _plugin
     _plugin.onNotification(Name, Subject, Text, Status, Priority, Sound, ImageFile)
 
+
 def onDisconnect(Connection):
     global _plugin
     _plugin.onDisconnect(Connection)
+
 
 def onHeartbeat():
     global _plugin
     _plugin.onHeartbeat()
 
     # Generic helper functions
+
+
 def DumpConfigToLog():
     for x in Parameters:
         if Parameters[x] != "":
-            Domoticz.Debug( "'" + x + "':'" + str(Parameters[x]) + "'")
+            Domoticz.Debug("'" + x + "':'" + str(Parameters[x]) + "'")
     Domoticz.Debug("Device count: " + str(len(Devices)))
     for x in Devices:
         Domoticz.Debug("Device:           " + str(x) + " - " + str(Devices[x]))
@@ -187,4 +202,3 @@ def DumpConfigToLog():
         Domoticz.Debug("Device sValue:   '" + Devices[x].sValue + "'")
         Domoticz.Debug("Device LastLevel: " + str(Devices[x].LastLevel))
     return
-
